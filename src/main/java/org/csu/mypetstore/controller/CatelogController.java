620e88e160779cd7b8ae4465163f2d12b52916be
赵 <992762582@qq.com>
2020-04-01 14:16:22 +0800

    Catelog

--- src/main/java/org/csu/mypetstore/controller/CatelogController.java
+++ src/main/java/org/csu/mypetstore/controller/CatelogController.java
@@ -1,14 +1,67 @@
 package org.csu.mypetstore.controller;
 
+import org.csu.mypetstore.domain.Category;
+import org.csu.mypetstore.domain.Item;
+import org.csu.mypetstore.domain.Product;
+import org.csu.mypetstore.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/catelog")
 public class CatelogController {
+    @Autowired
+    private CategoryService categoryService;
+
     @GetMapping("/view")
     public String view(){
         return "catelog/main";
+    }
+
+    @GetMapping("/viewCategory")
+    public String viewCategory(String categoryId, Model model){
+        if(categoryId != null){
+            Category category = categoryService.getCategory(categoryId);
+            List<Product> productList = categoryService.getProductListByCategory(categoryId);
+            model.addAttribute("category",category);
+            model.addAttribute("productList",productList);
+            return "catelog/category";
+        }
+        return "catelog/main";
+    }
+    @GetMapping("/viewProduct")
+    public String viewProduct(String productId, Model model) {
+        if (productId != null) {
+            List<Item> itemList = categoryService.getItemListByProduct(productId);
+            Product product = categoryService.getProduct(productId);
+            model.addAttribute("product", product);
+            model.addAttribute("itemList", itemList);
+        }
+        return "catelog/product";
+    }
+    @GetMapping("viewItem")
+    public String viewItem(String itemId, Model model){
+        Item item = categoryService.getItem(itemId);
+        Product product = item.getProduct();
+
+        processProductDescription(product);
+        model.addAttribute("item",item);
+        model.addAttribute("product",product);
+        return "catelog/item";
+    }
+    private void processProductDescription(Product product){
+        String [] temp = product.getDescription().split("\"");
+        product.setDescriptionImage(temp[1]);
+        product.setDescriptionText(temp[2].substring(1));
+    }
+    private void processProductDescription(List<Product> productList){
+        for(Product product : productList) {
+            processProductDescription(product);
+        }
     }
 }
--- src/test/java/org/csu/mypetstore/MypetstoreApplicationTests.java
+++ src/test/java/org/csu/mypetstore/MypetstoreApplicationTests.java
@@ -1,13 +1,41 @@
 package org.csu.mypetstore;
 
+import org.csu.mypetstore.domain.Category;
+import org.csu.mypetstore.domain.Product;
+import org.csu.mypetstore.persistence.CategoryMapper;
+import org.csu.mypetstore.service.CategoryService;
 import org.junit.jupiter.api.Test;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 @SpringBootTest
+@MapperScan("org.csu.mypetstore.persistence")
 class MypetstoreApplicationTests {
+    @Autowired
+    CategoryMapper categoryMapper;
+    @Autowired
+    CategoryService categoryService;
+
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void test(){
+
+        Category c = categoryService.getCategory("BIRDS");
+        System.out.println(c.getCategoryId()+" "+c.getName()+" "+c.getDescription());
+    }
+
+    @Test
+    void productTest(){
+        Product p = categoryService.getProduct("AV-CB-01");
+        List<Product> list = categoryService.getProductListByCategory("BIRDS");
+        System.out.println(list.size());
     }
 
 }
